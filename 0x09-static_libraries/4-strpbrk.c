09cec30643e43febb677a9a7654b3e5a8d0df4a4
Olise756 <isiakponaolise@gmail.com>
2022-11-09 19:26:37 -0800

    p

---
+++ 0x09-static_libraries/4-strpbrk.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+#include <stdio.h>
+
+/**
+ * _strpbrk - bytes
+ * @s: pointer to char
+ * @accept: pointer to char
+ * Return: NULL
+ */
+
+char *_strpbrk(char *s, char *accept)
+
+{
+	return (0);
+}
---
+++ 0x09-static_libraries/9-strcpy.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * *_strcpy - copy strg to another string
+ * @dest: pointer
+ * @src: poiter
+ * Return: numbers dest
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	return (0);
+}
